5ad81b82a5f0bfc57d079c4bee0968ef194f2851
kineticrun <44020958+kineticrun@users.noreply.github.com>
2025-11-13 19:09:30 +0100

    Szavak végén lévő írásjelek kezelése

--- pig_latin.py
+++ pig_latin.py
@@ -4,8 +4,17 @@ text = input("Kérem írjon be egy tetszőleges mondatot: ")
 output = []
 
 def format_to_pig_latin(text):
+    punctuation = ""
+    
+    if text[-1].isalpha() == False:
+        punctuation = text[-1]
+        text = text.replace(text[-1], "")
+
     if text[0] in vowels:
-        return text + "yay"
+        if punctuation == "":
+            return text + "yay"
+        else:
+            return text + "yay" + punctuation
     else:
         group = ""
         for i in text:
@@ -14,7 +23,10 @@ def format_to_pig_latin(text):
             else:
                 break
         result = text[len(group):len(text)]
-        result = result + group + "ay"
+        if punctuation == "":
+            result = result + group + "ay"
+        else:
+            result = result + group + "ay" + punctuation
         return result
 
 for word in text.split():
